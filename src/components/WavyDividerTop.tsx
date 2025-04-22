74533ce4fb8ad20b62597b182a8dcb5d8c42ddae
GraceMcArthur <graceeb24@gmail.com>
2025-04-23 11:40:36 +1200

    Update WavyDividerTop.tsx

--- src/components/WavyDividerTop.tsx
+++ src/components/WavyDividerTop.tsx
@@ -1,17 +1,3 @@
 
 
-const WavyDividerTop = () => (
-  <svg
-    viewBox="0 0 1440 100"
-    xmlns="http://www.w3.org/2000/svg"
-    className="w-full h-[100px]"
-    preserveAspectRatio="none"
-  >
-    <path
-      d="M0,0 C360,100 1080,0 1440,100 L1440,0 L0,0 Z"
-      fill="#FFF2F1" // TEMP: use red to test visibility
-    />
-  </svg>
-);
 
-export default WavyDividerTop;
