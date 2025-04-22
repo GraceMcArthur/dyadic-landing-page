4889006c0026aef94dc1c3e60109506cb8af5a45
GraceMcArthur <graceeb24@gmail.com>
2025-04-23 11:40:24 +1200

    Update WavyDividerBottom.tsx

--- src/components/WavyDividerBottom.tsx
+++ src/components/WavyDividerBottom.tsx
@@ -1,14 +1,1 @@
 
-
-const WavyDividerBottom = () => (
-  <svg
-    viewBox="0 0 1440 100"
-    xmlns="http://www.w3.org/2000/svg"
-    className="w-full h-[100px]"
-    preserveAspectRatio="none"
-  >
-    <path d="M0,0 C360,100 1080,0 1440,100 L1440,0 L0,0 Z" fill="white" />
-  </svg>
-);
-
-export default WavyDividerBottom;
